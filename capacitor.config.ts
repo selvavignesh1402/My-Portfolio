ccbccdc37b5ba411a4e9dd7e3e21a28bd73f5646
selvavignesh1402 <selvavigneshvijayan1402@gmail.com>
2024-07-13 23:54:08 +0530

    new2

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.portfolio.app',
+  appName: 'portfolio',
+  webDir: 'build'
+};
+
+export default config;
